9f6f4f09b561712fa7c45d019add196b1db3599d
get.dipen@gmail.com <get.dipen@gmail.com>
2022-09-15 23:09:23 -0700

    player selection completed

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ Live - https://overdrivemachines.github.io/spock-rock-game/
   - https://www.cssmatic.com/gradient-generator#'\-moz\-linear\-gradient\%28left\%2C\%20rgba\%28255\%2C0\%2C255\%2C0\.16\%29\%200\%25\%2C\%20rgba\%280\%2C255\%2C255\%2C0\.16\%29\%20100\%25\%29\%3B'
   - https://css-tricks.com/how-to-create-neon-text-with-css/
   - https://www.w3schools.com/howto/howto_css_glowing_text.asp
+- Switch Statement - https://www.w3schools.com/js/js_switch.asp
--- script.js
+++ script.js
@@ -8,6 +8,31 @@ function $(el) {
   }
 }
 
+const playerScoreEl = $("#playerScore");
+const playerChoiceEl = $("#playerChoice");
+
+const computerScoreEl = $("#computerScore");
+const computerChoiceEl = $("#computerChoice");
+
+const resultText = $("#resultText");
+
+// Player Icons
+const playerRock = $("#playerRock");
+const playerPaper = $("#playerPaper");
+const playerScissors = $("#playerScissors");
+const playerLizard = $("#playerLizard");
+const playerSpock = $("#playerSpock");
+
+// Computer Icons
+const computerRock = $("#computerRock");
+const computerPaper = $("#computerPaper");
+const computerScissors = $("#computerScissors");
+const computerLizard = $("#computerLizard");
+const computerSpock = $("#computerSpock");
+
+// Array of All font awesome icons
+const allGameIcons = document.querySelectorAll(".fa-regular");
+
 const choices = {
   rock: { name: "Rock", defeats: ["scissors", "lizard"] },
   paper: { name: "Paper", defeats: ["rock", "spock"] },
@@ -15,3 +40,41 @@ const choices = {
   lizard: { name: "Lizard", defeats: ["paper", "spock"] },
   spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
+
+// Reset all "selected" icons
+function resetSelected() {
+  allGameIcons.forEach((icon) => {
+    icon.classList.remove("selected");
+  });
+}
+
+// Passing player selection value and styling icons
+function select(playerChoice) {
+  // Remove "selected" class from all other icons
+  resetSelected();
+  // Add "selected" styling & playerChoice
+  switch (playerChoice) {
+    case "rock":
+      playerRock.classList.add("selected");
+      playerChoiceEl.textContent = " --- Rock";
+      break;
+    case "paper":
+      playerPaper.classList.add("selected");
+      playerChoiceEl.textContent = " --- Paper";
+      break;
+    case "scissors":
+      playerScissors.classList.add("selected");
+      playerChoiceEl.textContent = " --- Scissors";
+      break;
+    case "lizard":
+      playerLizard.classList.add("selected");
+      playerChoiceEl.textContent = " --- Lizard";
+      break;
+    case "spock":
+      playerSpock.classList.add("selected");
+      playerChoiceEl.textContent = " --- Spock";
+      break;
+    default:
+      break;
+  }
+}
