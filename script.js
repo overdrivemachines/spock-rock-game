e0a7c372697ba037562dd6c8a052bcb7624a0bdb
get.dipen@gmail.com <get.dipen@gmail.com>
2022-09-15 23:24:50 -0700

    Computer selection completed

--- script.js
+++ script.js
@@ -41,6 +41,8 @@ const choices = {
   spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
 
+let computerChoice = "";
+
 // Reset all "selected" icons
 function resetSelected() {
   allGameIcons.forEach((icon) => {
@@ -48,10 +50,62 @@ function resetSelected() {
   });
 }
 
-// Passing player selection value and styling icons
-function select(playerChoice) {
+// Random Computer Choice
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.random();
+
+  if (computerChoiceNumber < 0.2) {
+    computerChoice = "rock";
+  } else if (computerChoiceNumber <= 0.4) {
+    computerChoice = "paper";
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = "scissors";
+  } else if (computerChoiceNumber <= 0.8) {
+    computerChoice = "lizard";
+  } else {
+    computerChoice = "spock";
+  }
+}
+
+// Add "selected" styling & computerChoice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case "rock":
+      computerRock.classList.add("selected");
+      computerChoiceEl.textContent = " --- Rock";
+      break;
+    case "paper":
+      computerPaper.classList.add("selected");
+      computerChoiceEl.textContent = " --- Paper";
+      break;
+    case "scissors":
+      computerScissors.classList.add("selected");
+      computerChoiceEl.textContent = " --- Scissors";
+      break;
+    case "lizard":
+      computerLizard.classList.add("selected");
+      computerChoiceEl.textContent = " --- Lizard";
+      break;
+    case "spock":
+      computerSpock.classList.add("selected");
+      computerChoiceEl.textContent = " --- Spock";
+      break;
+    default:
+      break;
+  }
+}
+
+// Call functions to process turn
+function checkResult() {
   // Remove "selected" class from all other icons
   resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+}
+
+// Passing player selection value and styling icons
+function select(playerChoice) {
+  checkResult();
   // Add "selected" styling & playerChoice
   switch (playerChoice) {
     case "rock":
